56cde4d72e4c84c233d85a3aaa9c13596b9d6888
trydemo <nickjagtap56@gmail.com>
2024-06-29 11:56:00 +0200

    4

--- web_scraping.py
+++ web_scraping.py
@@ -1,6 +1,18 @@
 import openpyxl as xl
 wb = xl.load_workbook("transaction_id.xlsx")
 sheet = wb["Sheet1"]
-# cell = sheet['a1']
-cell1 = sheet.cell(1,1)
-print(cell1)
+cell = sheet['a1']
+cell = sheet.cell(1,1)
+
+
+for row in range (2, sheet.max_row + 1):
+    cell = sheet.cell(row, 3)
+    cell2 = cell.value
+    corrected_price = (cell2 * 0.9)
+    corrected_price_cell = sheet.cell(row, 5)
+    corrected_price_cell.value = corrected_price
+
+
+wb.save('transaction2.xlsx')
+
+
