f3c3570d09dafd300e1bde8010c3292665abd391
trydemo <nickjagtap56@gmail.com>
2024-06-29 12:06:07 +0200

    6

--- web_scraping.py
+++ web_scraping.py
@@ -7,6 +7,7 @@ sheet = wb["Sheet1"]
 # cell = sheet['a1']
 cell = sheet.cell(1,1)
 bold_font = Font(bold=True)
+sheet.delete_cols(3)
 
 
 
@@ -14,7 +15,7 @@ for row in range (2, sheet.max_row + 1):
     cell = sheet.cell(row, 3)
     cell2 = cell.value
     corrected_price = (cell2 * 0.9)
-    corrected_price_cell = sheet.cell(row, 5)
+    corrected_price_cell = sheet.cell(row, 4)
     corrected_price_cell.value = corrected_price
     corrected_price_cell.font = bold_font
 
